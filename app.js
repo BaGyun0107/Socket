3ae3d8fe160db5b42b8f6cd7659a0f359407e238
YOONSHIN PARK <pus1789@gmail.com>
2022-07-05 19:26:03 +0900

    카카오톡 모방 1대1 대화창 완성

--- app.js
+++ app.js
@@ -3,8 +3,23 @@ const http = require("http");
 const app = express();
 const path = require("path");
 const server = http.createServer(app);
+const socketIO = require("socket.io");
+const moment = require("moment");
 
-app.use(express.static(path.join(__dirname, "public")));
-const PORT = process.env.PORT || 4000;
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+  socket.on("chatting", (data) => {
+    const { name, msg } = data;
+    io.emit("chatting", {
+      name: name,
+      msg: msg,
+      time: moment(new Date()).format("h:mm A"),
+    });
+  });
+});
+
+app.use(express.static(path.join(__dirname, "src")));
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`server is running ${PORT}`));
